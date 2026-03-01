b84fe135c73e3a985e11139625e18da344c77f50
Zakee ahmad <ahmadzakee43@gmail.com>
2026-03-02 04:03:19 +0530

    Updated

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Student other = new Student();
-        System.out.println(other.name);
-        Student std = new Student();
-        System.out.println(std.name);
-        std.changeName("Waris alam"); // We can change the name via Function!
-        System.out.println(std.name); // Now this will return : Waris alam
+        Student std1 = new Student();
+        std1.Greet("Zakee ahmad"); // Run this code & see output :
+        std1.Student_Data("Zakee ahmad", 21, 98f);
+        int fact = std1.Factorial(5);
+        System.out.println("Factorial of the number is : " + fact); // Output : 120
+
+        Student std2 = new Student();  // We can create multiple objects by using same tamplate.
+        std2.Greet("Gaush alam"); // std2 also return the same output
     }
 
     static class Student {
@@ -13,30 +15,26 @@ public class Main {
         int age;
         float marks;
 
-        // Student() {
-        //     name = "Zakee ahmad";
-        //     age = 21;
-        //     marks = 98f;
-        // }
-
         Student() {
-            this("Zakee ahmad", 21, 98f);
-        }
-        // Student std1 = new Student("Gaush alam ,21,99f");
-        // Here this will replace with std1
-        Student(String name, int age, float marks) { // This is our constructor!
-            this.name = name;
-            this.age = age;
-            this.marks = marks;
+            this.name = "Zakee ahmad";
+            this.age = 21;
+            this.marks = 98f;
         }
 
-        // Same thing is working internally
-        Student(Student other) {
-            this.name = other.name;
+        void Greet(String name) { // Void function its not return any value
+            System.out.println("Have a nice day! " + name);
         }
 
-        void changeName(String name) {
-            this.name = name;
+        void Student_Data(String name, int age, float marks) { // This function pass all the data from Student
+            System.out.println("Name : " + name + "\n" + "Age : " + age + "\n" + "marks : " + marks);
+        }
+
+        int Factorial(int n) { // This function return the the data!
+            int fact = 1;
+            for (int i = 1; i <= n; i++) {
+                fact *= i;
+            }
+            return fact;
         }
     }
 }
